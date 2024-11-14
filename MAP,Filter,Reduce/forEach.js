962bdc2c0b4dbde437c607783f1e0020deadbd82
Abhimanyu Kumar <88124067+abhimanyu7062@users.noreply.github.com>
2024-11-14 23:22:27 +0530

    Create forEach.js

---
+++ MAP,Filter,Reduce/forEach.js
@@ -0,0 +1,6 @@
+// forEach -  Used for iterating over an array & perform side effects (like logging or modifying external variables) and
+//you don't need a return value.
+
+let number = [1,2,3,4]
+number.forEach((num) =>{
+  console.log(num*2))
